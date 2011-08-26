efc8ca9c29a57209642201c20c612409b0a319d4
Paul Gross <pgross@gmail.com>
2011-08-26 17:02:26 -0500

    Added a before function to clear out the playlist.

--- spec/jukebox_web/models/playlist_spec.clj
+++ spec/jukebox_web/models/playlist_spec.clj
@@ -2,25 +2,32 @@
   (:require [jukebox-web.models.playlist :as playlist])
   (:use [speclj.core]))
 
-(describe "add-random-song!"
-  (it "adds a random song to the queued songs"
-      (playlist/reset-state!)
-      (should (empty? (playlist/queued-songs)))
-      (playlist/add-random-song!)
-      (should= 1 (count (playlist/queued-songs)))))
+(describe "playlist"
+  (before
+    (playlist/reset-state!))
 
-(describe "set-current-song!"
-  (it "sets the current song to a random song if the queue is empty"
-      (playlist/reset-state!)
-      (should (empty? (playlist/queued-songs)))
-      (playlist/set-current-song!)
-      (should-not= nil (playlist/current-song)))
+  (describe "add-random-song!"
+    (it "adds a random song to the queued songs"
+        (should (empty? (playlist/queued-songs)))
+        (playlist/add-random-song!)
+        (should= 1 (count (playlist/queued-songs))))
 
-  (it "sets the current song to to the first song in the queue if the queue is not empty"
-      (playlist/reset-state!)
-      (playlist/add-random-song!)
-      (let [next-in-queue (playlist/next-song)]
+    (it "adds the song to the end of the queue"
+        (playlist/add-random-song!)
+        (let [first-value (playlist/next-song)]
+          (playlist/add-random-song!)
+          (should= first-value (playlist/next-song)))))
+
+  (describe "set-current-song!"
+    (it "sets the current song to a random song if the queue is empty"
+        (should (empty? (playlist/queued-songs)))
         (playlist/set-current-song!)
-        (should= next-in-queue (playlist/current-song)))))
+        (should-not= nil (playlist/current-song)))
+
+    (it "sets the current song to to the first song in the queue if the queue is not empty"
+        (playlist/add-random-song!)
+        (let [next-in-queue (playlist/next-song)]
+          (playlist/set-current-song!)
+          (should= next-in-queue (playlist/current-song))))))
 
 (run-specs)
