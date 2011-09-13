89609b022b20bb1fefc76e89408ff6f5b7dff819
Tony Pitluga <tony.pitluga@gmail.com>
2011-09-12 19:53:56 -0500

    pulled player code into its own file

--- src/jukebox_player/example.clj
+++ src/jukebox_player/example.clj
@@ -1,47 +1,9 @@
 (ns jukebox-player.example
-  (:import [javax.sound.sampled AudioSystem])
   (:use [jukebox-player.playable]
+        [jukebox-player.player]
         [jukebox-player.mp4-track :as mp4]
         [jukebox-player.basic-track :as basic]))
 
-(def buffer-size 4096)
-(def player-state (atom :stop))
-
-(defn load-track [file]
-  (if (re-matches #".*\.m4a$" file)
-    (mp4/load-mp4-track file)
-    (basic/load-basic-track file)))
-
-(defn- build-line-out [playable]
-  (AudioSystem/getSourceDataLine (out-format playable)))
-
-(defn play-track [playable]
-  (let [speaker (build-line-out playable)
-        audio-stream (in-stream playable)
-        buffer (byte-array buffer-size)]
-    (doto speaker (.open) (.start))
-    (loop [bytes-read (.read audio-stream buffer)]
-      (condp = @player-state
-        :play (when-not (= bytes-read -1)
-                (.write speaker buffer 0 bytes-read)
-                (recur (.read audio-stream buffer)))
-        :pause (do (Thread/sleep 100) (recur bytes-read))
-        "do nothing"))
-     (doto speaker (.close))))
-
-
-(defn start [files]
-  (loop [files-to-play files]
-    (when-let [file (first files-to-play)]
-      (cond
-        (= :play @player-state) (do (play-track (load-track file)) (recur (rest files-to-play)))
-        (= :stop @player-state) (do (Thread/sleep 100) (recur files-to-play))
-        (= :skip @player-state) (do (reset! player-state :play) (recur (rest files-to-play)))))))
-
-(defn pause [] (reset! player-state :pause))
-(defn play  [] (reset! player-state :play))
-(defn skip  [] (reset! player-state :skip))
-(defn stop  [] (reset! player-state :stop))
 
 (defn -main [& files]
   (let [player (Thread. #(start files))]
