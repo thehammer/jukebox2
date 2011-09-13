31be85b325b94457467f1af07940a11455901405
Tony Pitluga <tony.pitluga@gmail.com>
2011-09-12 19:40:33 -0500

    and we have threading

--- src/jukebox_player/example.clj
+++ src/jukebox_player/example.clj
@@ -5,23 +5,52 @@
         [jukebox-player.basic-track :as basic]))
 
 (def buffer-size 4096)
+(def player-state (atom :stop))
 
 (defn load-track [file]
   (if (re-matches #".*\.m4a$" file)
     (mp4/load-mp4-track file)
     (basic/load-basic-track file)))
 
-(defn play [playable]
-  (let [speaker (AudioSystem/getSourceDataLine (out-format playable))
+(defn- build-line-out [playable]
+  (AudioSystem/getSourceDataLine (out-format playable)))
+
+(defn play-track [playable]
+  (let [speaker (build-line-out playable)
         audio-stream (in-stream playable)
         buffer (byte-array buffer-size)]
     (doto speaker (.open) (.start))
     (loop [bytes-read (.read audio-stream buffer)]
-      (when-not (= bytes-read -1)
-        (.write speaker buffer 0 bytes-read)
-        (recur (.read audio-stream buffer))))
+      (condp = @player-state
+        :play (when-not (= bytes-read -1)
+                (.write speaker buffer 0 bytes-read)
+                (recur (.read audio-stream buffer)))
+        :pause (do (Thread/sleep 100) (recur bytes-read))
+        "do nothing"))
      (doto speaker (.close))))
 
-(defn -main [file & args]
-  (let [track (load-track file)]
-    (play track)))
+
+(defn start [files]
+  (loop [files-to-play files]
+    (when-let [file (first files-to-play)]
+      (cond
+        (= :play @player-state) (do (play-track (load-track file)) (recur (rest files-to-play)))
+        (= :stop @player-state) (do (Thread/sleep 100) (recur files-to-play))
+        (= :skip @player-state) (do (reset! player-state :play) (recur (rest files-to-play)))))))
+
+(defn pause [] (reset! player-state :pause))
+(defn play  [] (reset! player-state :play))
+(defn skip  [] (reset! player-state :skip))
+(defn stop  [] (reset! player-state :stop))
+
+(defn -main [& files]
+  (let [player (Thread. #(start files))]
+    (.start player)
+    (play)
+    (Thread/sleep 10000)
+    (pause)
+    (Thread/sleep 10000)
+    (play)
+    (Thread/sleep 10000)
+    (stop)
+    (.join player)))
