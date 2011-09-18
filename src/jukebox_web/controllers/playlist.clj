ba090a7201309a14beb52fafafb3fce2e667497b
Tony Pitluga <tony.pitluga@gmail.com>
2011-09-18 17:31:22 -0500

    show current skip count for logged in users

--- src/jukebox_web/controllers/playlist.clj
+++ src/jukebox_web/controllers/playlist.clj
@@ -1,9 +1,12 @@
 (ns jukebox-web.controllers.playlist
   (:require [jukebox-web.views.playlist :as view]
-            [jukebox-web.models.playlist :as playlist]))
+            [jukebox-web.models.playlist :as playlist]
+            [jukebox-web.models.user :as user]))
 
 (defn index [request]
-  (view/index (-> request :session :current-user) (playlist/current-song) (playlist/queued-songs)))
+  (let [login (-> request :session :current-user)
+        current-user (user/find-by-login login)]
+    (view/index current-user (playlist/current-song) (playlist/queued-songs))))
 
 (defn add-one [request]
   (playlist/add-random-song!)
--- src/jukebox_web/views/playlist.clj
+++ src/jukebox_web/views/playlist.clj
@@ -15,24 +15,21 @@
       ")"]))
 
 (defn index [current-user current-song queued-songs]
-  (layout/main "Playlist"
-     [:h3 "Current Song"]
-     [:p (display-song current-song)]
-     [:h3 "Queued Songs"]
-     [:ul (map #(vector :li (display-song %)) queued-songs)]
-     [:h3 "Operations"]
-     [:ul
-      [:li (link-to "/player/play" "Play")]
-      [:li (link-to "/player/pause" "Pause")]
-      [:li (link-to "/player/skip" "Skip")]
-      [:li (link-to "/playlist/add-one" "Add random track")]]
-     [:h3 "Users"]
-     [:ul
-      (when-not (nil? current-user)
-        [:li "logged in as: " current-user])
-      [:li (link-to "/users/sign-up" "Sign Up")]
-      [:li (link-to "/users/sign-in" "Sign In")]
-      [:li
-       (form-to [:post "/users/sign-out"]
-         (submit-button "Sign Out"))]
-  ]))
+  (let [{:keys [login skip-count]} current-user]
+    (layout/main "Playlist"
+       [:h3 "Current Song"]
+       [:p (display-song current-song)]
+       [:h3 "Queued Songs"]
+       [:ul (map #(vector :li (display-song %)) queued-songs)]
+       [:h3 "Operations"]
+       [:ul
+        [:li (link-to "/player/play" "Play")]
+        [:li (link-to "/player/pause" "Pause")]
+        [:li (link-to "/player/skip" "Skip")]
+        [:li (link-to "/playlist/add-one" "Add random track")]]
+       [:h3 "Users"]
+       [:ul
+        (when-not (nil? current-user) [:li (format "logged in as: %s (skips: %s)" login skip-count)])
+        (when-not (nil? current-user) [:li (form-to [:post "/users/sign-out"] (submit-button "Sign Out"))])
+        [:li (link-to "/users/sign-up" "Sign Up")]
+        [:li (link-to "/users/sign-in" "Sign In")]])))
