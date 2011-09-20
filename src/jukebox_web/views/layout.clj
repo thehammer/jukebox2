e39ed1a86d21e977f5aba7a6d44d7f287ba31237
Tony Pitluga <tony.pitluga@gmail.com>
2011-09-20 12:06:17 -0500

    added jquery and a stylesheet

--- src/jukebox_web/views/layout.clj
+++ src/jukebox_web/views/layout.clj
@@ -5,6 +5,8 @@
   (html5
     [:head
      [:title title]
+     [:script {:src "/js/jquery-1.6.4.min.js"}]
+     [:script {:src "/js/jquery.multifile-1.47.min.js"}]
      (include-css "/css/style.css")]
     [:body
       content]))
