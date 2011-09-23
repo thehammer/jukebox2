4828742989e9f3521856c97b661d3bda4c74fb57
Tony and Phinze <devs+jukebox@braintreepayments.com>
2011-09-23 10:24:09 -0500

    moved methods from library controller to library model

--- resources/public/js/upload.js
+++ resources/public/js/upload.js
@@ -2,7 +2,7 @@ $(function() {
   $("#uploader").pluploadQueue({
     runtimes : 'flash,html5',
     url : '/library/upload/tony',
-    max_file_size : '10mb',
+    max_file_size : '20mb',
     unique_names : true,
     filters : [{title : "Music files", extensions : "mp3,m4a"}],
     flash_swf_url : '/plupload.flash.swf',
---
+++ src/jukebox_web/models/library.clj
@@ -0,0 +1,33 @@
+(ns jukebox-web.models.library
+  (:import [java.io File]
+           [java.util UUID])
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s])
+  (:use [jukebox-player.tags]))
+
+
+(def *music-library* "music")
+
+(defn- file-path [& parts]
+  (s/join File/separator parts))
+
+(defn- mkdir-p [path]
+  (.mkdirs (io/as-file path)))
+
+(defn- mv [from to]
+  (.renameTo (io/as-file from) (io/as-file to)))
+
+(defn extension [filename]
+  (last (s/split (str filename) #"\.")))
+
+(defn- rename-with-tags [user file]
+  (let [{:keys [artist album title]} (extract-tags file)
+        dir (file-path *music-library* user artist album)
+        new-file (file-path dir (str title "." (extension file)))]
+    (mkdir-p dir)
+    (mv file new-file)))
+
+(defn save-file [tempfile user ext]
+  (let [file-with-ext (io/as-file (file-path *music-library* (str (UUID/randomUUID) "." ext)))]
+    (io/copy tempfile file-with-ext)
+    (rename-with-tags user file-with-ext)))
