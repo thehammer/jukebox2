e52c2813e5f8bde16e379e7d03e99c32eb48ceca
Paul Gross <pgross@gmail.com>
2011-10-11 21:59:16 -0500

    Upgrade speclj to 1.5.2

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.mindrot/jbcrypt "0.3m"]]
   :dev-dependencies [[lein-ring "0.4.5"]
                      [lein-javac "1.2.1-SNAPSHOT"]
-                     [speclj "1.5.1"]
+                     [speclj "1.5.2"]
                      [speclj-growl "1.0.0-SNAPSHOT"]]
   :java-source-path [["src/jukebox_player"]]
   :ring {:handler jukebox-web.core/app}
