9549a3565c834531ce02ff5b336e253426c6b6f9
brian and tony <code@getbraintree.com>
2011-10-28 14:20:02 -0500

    removed earmuffs from playlist atoms and extracted recent-songs-factor into a def

--- src/jukebox_web/models/playlist.clj
+++ src/jukebox_web/models/playlist.clj
@@ -1,49 +1,51 @@
 (ns jukebox-web.models.playlist
   (:require [jukebox-web.models.library :as library]))
 
-(def *current-song* (atom nil))
-(def *queued-songs* (atom []))
-(def recent-songs (atom clojure.lang.PersistentQueue/EMPTY))
+(def current-song-atom (atom nil))
+(def queued-songs-atom (atom []))
+(def recent-songs-atom (atom clojure.lang.PersistentQueue/EMPTY))
+
+(def *recent-songs-factor* 0.25)
 
 (defn- random-song []
   (let [music-file (rand-nth (library/all-tracks))]
     (.getPath music-file)))
 
 (defn current-song []
-  @*current-song*)
+  @current-song-atom)
 
 (defn queued-songs []
-  @*queued-songs*)
+  @queued-songs-atom)
 
 (defn- recent-songs-to-keep []
-  (* (count (library/all-tracks)) 0.25))
+  (* (count (library/all-tracks)) *recent-songs-factor*))
 
 (defn add-song! [song]
-  (swap! *queued-songs* conj (library/file-on-disk song))
-  (swap! recent-songs conj song)
-  (if (< (recent-songs-to-keep) (count @recent-songs))
-    (swap! recent-songs pop)))
+  (swap! queued-songs-atom conj (library/file-on-disk song))
+  (swap! recent-songs-atom conj song)
+  (if (< (recent-songs-to-keep) (count @recent-songs-atom))
+    (swap! recent-songs-atom pop)))
 
 (defn add-random-song! []
   (loop [song (random-song)]
-    (if (.contains @recent-songs song)
+    (if (.contains @recent-songs-atom song)
       (recur (random-song))
       (add-song! song))))
 
 (defn reset-state! []
-  (reset! *current-song* nil)
-  (reset! *queued-songs* [])
-  (reset! recent-songs (clojure.lang.PersistentQueue/EMPTY)))
+  (reset! current-song-atom nil)
+  (reset! queued-songs-atom [])
+  (reset! recent-songs-atom (clojure.lang.PersistentQueue/EMPTY)))
 
 (defn- move-to-next-track! []
-  (reset! *current-song* (first @*queued-songs*))
-  (swap! *queued-songs* (comp vec rest)))
+  (reset! current-song-atom (first @queued-songs-atom))
+  (swap! queued-songs-atom (comp vec rest)))
 
 (defn next-track [_]
-  (if-let [queued (first @*queued-songs*)]
+  (if-let [queued (first @queued-songs-atom)]
     (move-to-next-track!)
     (do (add-random-song!) (move-to-next-track!)))
-  @*current-song*)
+  @current-song-atom)
 
 (defn playlist-seq []
   (iterate next-track (next-track "")))
