e47944e9ff7117f9b08c70b966a2c6cb28376553
cory <cory.monty@gmail.com>
2011-11-01 21:25:47 -0500

    Refactors player response to private method

--- src/jukebox_web/controllers/player.clj
+++ src/jukebox_web/controllers/player.clj
@@ -6,8 +6,7 @@
     [jukebox-web.models.user :as user]
     [jukebox-web.models.playlist :as playlist]))
 
-(defn play [request]
-  (player/play!)
+(defn- respond-to [request]
   (let [song (playlist/current-song)
         loggedin (not (nil? (-> request :session :current-user)))
         progress (int (player/current-time))]
@@ -15,23 +14,17 @@
     (json/response (merge (extract-tags song) {:progress progress :playing (player/playing?) :canSkip loggedin}))
     {:status 302 :headers {"Location" "/playlist"}})))
 
+(defn play [request]
+  (player/play!)
+  (respond-to request))
+
 (defn pause [request]
   (player/pause!)
-  (let [song (playlist/current-song)
-        loggedin (not (nil? (-> request :session :current-user)))
-        progress (int (player/current-time))]
-  (if (json/request? ((:headers request) "accept"))
-    (json/response (merge (extract-tags song) {:progress progress :playing (player/playing?) :canSkip loggedin}))
-    {:status 302 :headers {"Location" "/playlist"}})))
+  (respond-to request))
 
 (defn skip [request]
   (when-let [current-user (-> request :session :current-user)]
     (player/skip!)
     (do (Thread/sleep 1000))
     (user/increment-skip-count! current-user))
-  (let [song (playlist/current-song)
-        loggedin (not (nil? (-> request :session :current-user)))
-        progress (int (player/current-time))]
-  (if (json/request? ((:headers request) "accept"))
-    (json/response (merge (extract-tags song) {:progress progress :playing (player/playing?) :canSkip loggedin}))
-    {:status 302 :headers {"Location" "/playlist"}})))
+  (respond-to request))
