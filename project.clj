cef99bb105dc3b04461c73109e8761a74f92417e
Steven Deobald <steven@c42.in>
2012-03-17 17:15:00 +0530

    taking out java compilation for lein 1.7 compatibility.

--- project.clj
+++ project.clj
@@ -24,7 +24,6 @@
                      [lein-javac "1.2.1-SNAPSHOT"]
                      [speclj "1.5.2"]
                      [speclj-growl "1.0.0-SNAPSHOT"]]
-  :java-source-path [["src/jukebox_player"]]
   :main jukebox-web.core
   :ring {:handler jukebox-web.core/app}
   :test-path "spec/")
